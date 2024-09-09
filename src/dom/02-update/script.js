dc62c8898d960bf72b13cef890ddfba98edb352c
surajverma2587 <surajverma2587@gmail.com>
2024-09-09 10:20:55 +0100

    update DOM

---
+++ src/dom/02-update/script.js
@@ -0,0 +1,11 @@
+// target my h1 element
+const h1Element = document.querySelector(".title");
+
+// update the text content to uppercase
+h1Element.textContent = h1Element.textContent.toUpperCase();
+
+// target my h2 element
+const h2Element = document.querySelector(".sub-title");
+
+// update the text content to uppercase
+h2Element.textContent = h2Element.textContent.toUpperCase();
