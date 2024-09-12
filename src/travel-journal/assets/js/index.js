a2d8e60fd2bab61ab61d2a1c82f85d4f7dcdeb8c
surajverma2587 <surajverma2587@gmail.com>
2024-09-12 10:44:54 +0100

    add submit logic and update destinations

--- src/travel-journal/assets/js/index.js
+++ src/travel-journal/assets/js/index.js
@@ -35,6 +35,44 @@ const handleSubmit = (event) => {
   destinationInput.value = "";
   imageUrlInput.value = "";
   descriptionInput.value = "";
+
+  render();
+};
+
+// create alert function
+const createAlert = () => {
+  const divElement = document.createElement("div");
+  divElement.setAttribute("class", "alert alert-info text-center");
+  divElement.setAttribute("role", "alert");
+  divElement.setAttribute("id", "alert");
+
+  divElement.textContent =
+    " You have no destinations. Please use the form above to live your dreams...";
+
+  return divElement;
+};
+
+const renderAlert = () => {
+  // create alert
+  const alert = createAlert();
+
+  // render the alert
+  const destinationsSection = document.getElementById("destinations-section");
+
+  destinationsSection.append(alert);
+};
+
+const render = () => {
+  // render alert if empty
+  if (destinations.length === 0) {
+    renderAlert();
+  }
+  // render cards if not empty
+  else {
+    // remove alert if present
+    document.getElementById("alert").remove();
+    console.log("BUILD CARDS IN JS FROM ARRAY");
+  }
 };
 
 // target form element
@@ -42,3 +80,5 @@ const destinationForm = document.getElementById("destination-form");
 
 // add submit event listener
 destinationForm.addEventListener("submit", handleSubmit);
+
+render();
