2f227dac94823a475022fab96e34498b901e35a6
surajverma2587 <surajverma2587@gmail.com>
2024-09-12 11:30:01 +0100

    add cards in js

--- src/travel-journal/assets/js/index.js
+++ src/travel-journal/assets/js/index.js
@@ -62,6 +62,60 @@ const renderAlert = () => {
   destinationsSection.append(alert);
 };
 
+// create card
+const createCard = ({ imageUrl, destination }) => {
+  const cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card destination-card m-3");
+
+  // img
+  const cardImg = document.createElement("img");
+  cardImg.setAttribute("src", imageUrl);
+  cardImg.setAttribute("class", "card-img-top");
+  cardImg.setAttribute("alt", destination);
+
+  // card body
+  const cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
+
+  // h5
+  const cardTitle = document.createElement("h5");
+  cardTitle.setAttribute("class", "card-title");
+  cardTitle.textContent = destination;
+
+  // button
+  const cardButton = document.createElement("button");
+  cardButton.setAttribute("class", "btn btn-outline-primary w-100");
+  cardButton.textContent = "View";
+
+  cardBody.append(cardTitle, cardButton);
+  cardDiv.append(cardImg, cardBody);
+
+  return cardDiv;
+};
+
+// render cards
+const renderCards = () => {
+  // create parent div
+  const parentDiv = document.createElement("div");
+  parentDiv.setAttribute("id", "destinations-container");
+  parentDiv.setAttribute(
+    "class",
+    "d-flex flex-row flex-wrap justify-content-evenly"
+  );
+
+  // loop through each destination and create and render each card
+  destinations.forEach((destination) => {
+    // create the destination card
+    const destinationCard = createCard(destination);
+
+    // append the card to the parent
+    parentDiv.append(destinationCard);
+  });
+
+  // append parent div with cards to section
+  document.getElementById("destinations-section").append(parentDiv);
+};
+
 const render = () => {
   // render alert if empty
   if (destinations.length === 0) {
@@ -70,8 +124,12 @@ const render = () => {
   // render cards if not empty
   else {
     // remove alert if present
-    document.getElementById("alert").remove();
-    console.log("BUILD CARDS IN JS FROM ARRAY");
+    document.getElementById("alert")?.remove();
+
+    // remove cards if present
+    document.getElementById("destinations-container")?.remove();
+
+    renderCards();
   }
 };
 
