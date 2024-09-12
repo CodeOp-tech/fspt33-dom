5714af44bb604d6fcd65a2362e87177a7d79b031
surajverma2587 <surajverma2587@gmail.com>
2024-09-12 10:22:41 +0100

    styled form and logic

--- src/travel-journal/assets/js/index.js
+++ src/travel-journal/assets/js/index.js
@@ -1,1 +1,44 @@
-console.log("hello from JS");
+// define an array to store destinations
+const destinations = [];
+
+// define a submit event handler
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const destinationInput = document.getElementById("destination");
+
+  const imageUrlInput = document.getElementById("imageUrl");
+
+  const descriptionInput = document.getElementById("description");
+
+  // get destination value
+  const destination = destinationInput.value;
+
+  // get imageUrl value
+  const imageUrl = imageUrlInput.value;
+
+  // get description value
+  const description = descriptionInput.value;
+
+  // create an object
+  const destinationObject = {
+    destination,
+    imageUrl,
+    description,
+  };
+
+  destinations.push(destinationObject);
+
+  console.log(destinations);
+
+  // clear form fields
+  destinationInput.value = "";
+  imageUrlInput.value = "";
+  descriptionInput.value = "";
+};
+
+// target form element
+const destinationForm = document.getElementById("destination-form");
+
+// add submit event listener
+destinationForm.addEventListener("submit", handleSubmit);
